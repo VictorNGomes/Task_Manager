1ff25322f110af4138926b5faa30837ce1121204
Victor Gomes <victor.gomes.091@ufrn.edu.br>
2023-10-31 10:45:56 -0300

    change priority ok

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import psutil
 def listar_processos():
     process_list.delete(*process_list.get_children())
     processes = []
-    for proc in psutil.process_iter(attrs=['pid', 'name', 'status', 'cpu_percent', 'num_threads', 'username']):
-        processes.append(proc.info)
+    filtro = nome_filtro.get().lower()  # Obtém o valor do filtro em letras minúsculas
+    for proc in psutil.process_iter(attrs=['pid', 'name', 'status', 'cpu_percent', 'num_threads', 'username', 'nice']):
+        process_info = proc.info
+        if filtro in process_info['name'].lower():  # Verifica se o filtro está contido no nome do processo
+            processes.append(process_info)
 
     # Ordene a lista de processos com base no uso de CPU (cpu_percent)
     processes.sort(key=lambda x: x['cpu_percent'], reverse=True)
@@ -18,14 +21,10 @@ def listar_processos():
         cpu_percent = proc['cpu_percent']
         num_threads = proc['num_threads']
         username = proc['username']
-
-        if nome_filtro.get() in name:  # Verifica se o nome do processo contém o filtro
-            process_list.insert("", "end", values=(pid, name, status, cpu_percent, num_threads, username))
+        nice = proc['nice']
+        process_list.insert("", "end", values=(pid, name, status, cpu_percent, num_threads, username, nice))
 
     app.after(5000, listar_processos)  # Atualiza a cada 5 segundos (5000 milissegundos)
-
-def filtrar_processos():
-    listar_processos()  # Reexecute a função listar_processos para aplicar o filtro
 
 def matar_processo():
     selected_item = process_list.selection()
@@ -50,6 +49,15 @@ def parar_continuar_processo(acao):
         except psutil.NoSuchProcess:
             pass
 
+def alterar_prioridade():
+    pid = pid_entry.get()
+    nice = int(prioridade_var.get())  # Obtém o valor da prioridade do menu suspenso
+    try:
+        process = psutil.Process(int(pid))
+        process.nice(nice)
+    except (psutil.NoSuchProcess, ValueError):
+        pass
+
 def ordenar_por_cpu():
     listar_processos()  # Atualize a lista ao clicar na coluna "CPU%"
 
@@ -58,22 +66,44 @@ app.title("Gerenciador de Tarefas")
 
 process_list_frame = ttk.Frame(app)
 process_list_frame.pack(padx=10, pady=10)
-process_list = ttk.Treeview(process_list_frame, columns=("PID", "Nome", "Status", "CPU%", "Threads", "Usuário"), show="headings")
+process_list = ttk.Treeview(process_list_frame, columns=("PID", "Nome", "Status", "CPU%", "Threads", "Usuário", "Nice"), show="headings")
 process_list.heading("PID", text="PID")
 process_list.heading("Nome", text="Nome")
 process_list.heading("Status", text="Status")
 process_list.heading("CPU%", text="CPU%", command=ordenar_por_cpu)
 process_list.heading("Threads", text="Threads")
 process_list.heading("Usuário", text="Usuário")
+process_list.heading("Nice", text="Nice")
 process_list.pack()
 
 filtro_frame = ttk.Frame(app)
 filtro_frame.pack(padx=10, pady=10)
 nome_filtro = tk.StringVar()  # Variável para armazenar o texto do filtro
+filtro_label = ttk.Label(filtro_frame, text="Filtrar por nome:")
+filtro_label.pack(side=tk.LEFT)
 filtro_entry = ttk.Entry(filtro_frame, textvariable=nome_filtro, width=30)
 filtro_entry.pack(side=tk.LEFT)
-filtro_button = ttk.Button(filtro_frame, text="Filtrar", command=filtrar_processos)
+filtro_button = ttk.Button(filtro_frame, text="Filtrar", command=listar_processos)
 filtro_button.pack(side=tk.LEFT)
+
+pid_frame = ttk.Frame(app)
+pid_frame.pack(padx=10, pady=10)
+pid_label = ttk.Label(pid_frame, text="Digite o PID:")
+pid_label.pack(side=tk.LEFT)
+pid_entry = ttk.Entry(pid_frame, width=10)
+pid_entry.pack(side=tk.LEFT)
+
+prioridade_frame = ttk.Frame(app)
+prioridade_frame.pack(padx=10, pady=10)
+prioridade_label = ttk.Label(prioridade_frame, text="Nova Prioridade:")
+prioridade_label.pack(side=tk.LEFT)
+prioridade_var = tk.StringVar()
+prioridade_combobox = ttk.Combobox(prioridade_frame, textvariable=prioridade_var, values=["-20", "-10", "0", "10", "20"])
+prioridade_combobox.set("0")  # Defina o valor padrão
+prioridade_combobox.pack(side=tk.LEFT)
+
+prioridade_button = ttk.Button(prioridade_frame, text="Alterar Prioridade", command=alterar_prioridade)
+prioridade_button.pack(side=tk.LEFT)
 
 button_frame = tk.Frame(app)
 button_frame.pack(pady=10)
